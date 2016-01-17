613b04d83fb589325659d17355dd3ef10bd8fce8
arun-prasath <arunprasath-dev@outlook.com>
2016-01-17 20:40:02 +0530

    Added features for message source to read values from property file and display it in jsp pages

--- fitnesstracker/src/main/java/com/mysite/fitnesstracker/controller/MinutesController.java
+++ fitnesstracker/src/main/java/com/mysite/fitnesstracker/controller/MinutesController.java
@@ -3,6 +3,7 @@
  */
 package com.mysite.fitnesstracker.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +16,13 @@ import com.mysite.fitnesstracker.model.Exercise;
  */
 @Controller
 public class MinutesController {
+	
+	private static final Logger logger = Logger.getLogger(MinutesController.class);
 
 	@RequestMapping("/addMinutes")
 	public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
 		exercise.setMinutes(5);
+		logger.info(exercise.getMinutes() + " minutes");
 		return "minutes";
 	}
 }
