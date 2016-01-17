b2b251470e8830678287a2a375177b84294da4e2
arun-prasath <arunprasath-dev@outlook.com>
2016-01-17 18:39:32 +0530

    Added around advice for more powerful aspectj feature for logging

--- fitnesstracker/src/main/java/com/mysite/fitnesstracker/aspect/TracingAspect.java
+++ fitnesstracker/src/main/java/com/mysite/fitnesstracker/aspect/TracingAspect.java
@@ -4,10 +4,9 @@
 package com.mysite.fitnesstracker.aspect;
 
 import org.apache.log4j.Logger;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 
 /**
  * @author Arun J
@@ -20,23 +19,27 @@ public class TracingAspect {
 	Logger logger = Logger.getLogger(TracingAspect.class);
 	
 	
-	@Before("execution(* *(..))")
-	public void entering(JoinPoint joinPoint) {
-		logger.info("Entering -->> " + joinPoint.getStaticPart().getSignature().toString());
-	}
-	
-	@After("execution(* *(..))")
-	public void exiting(JoinPoint joinPoint) {
-		Object[] args = joinPoint.getArgs();
-		
-		if(args.length > 0) {
-			logger.info("Arguments from -->> " + joinPoint.getStaticPart().getSignature().toString() + ":");
-			for (Object arg : joinPoint.getArgs()) {
-				logger.info(joinPoint.getStaticPart().getSignature().toShortString() + " >> " + arg);
+	@Around("execution(* (@org.springframework.stereotype.Controller *).*(..))")
+	public Object catchException(ProceedingJoinPoint proceedingJoinPoint) {
+		logger.info("Entering -->> " + proceedingJoinPoint.getStaticPart().getSignature().toString());
+		try {
+			return proceedingJoinPoint.proceed();
+		} catch (Throwable e) {
+			logger.error("An exception occurred in " + proceedingJoinPoint.getStaticPart().getSignature().toString());
+			logger.error("Exception message is: " + e.getMessage());
+		} finally {
+			Object[] args = proceedingJoinPoint.getArgs();
+			
+			if(args.length > 0) {
+				logger.info("Arguments from -->> " + proceedingJoinPoint.getStaticPart().getSignature().toString() + ":");
+				for (Object arg : proceedingJoinPoint.getArgs()) {
+					logger.info(proceedingJoinPoint.getStaticPart().getSignature().toShortString() + " >> " + arg);
+				}
 			}
+			
+			logger.info("Exiting -->> " + proceedingJoinPoint.getStaticPart().getSignature().toString());
 		}
-		
-		logger.info("Exiting -->> " + joinPoint.getStaticPart().getSignature().toString());
+		return null;
 	}
 
 }
