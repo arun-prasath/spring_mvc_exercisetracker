45f19b72c88db49095b1b6b16f99cbece939a6bd
arun-prasath <arunprasath-dev@outlook.com>
2016-01-17 11:44:32 +0530

    Added logging to controllers

--- fitnesstracker/src/main/java/com/mysite/fitnesstracker/controller/HelloController.java
+++ fitnesstracker/src/main/java/com/mysite/fitnesstracker/controller/HelloController.java
@@ -3,6 +3,7 @@
  */
 package com.mysite.fitnesstracker.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 	
+	private static final Logger logger = Logger.getLogger(HelloController.class);
+	
 	@RequestMapping("/hello")
 	public String sayHello(Model model) {
+		logger.info("Entering HelloController.sayHello");
 		model.addAttribute("name", "Arun");
+		logger.info("Exiting HelloController.sayHello");
 		return "hello";
 	}
 
--- fitnesstracker/src/main/java/com/mysite/fitnesstracker/controller/MinutesController.java
+++ fitnesstracker/src/main/java/com/mysite/fitnesstracker/controller/MinutesController.java
@@ -3,6 +3,7 @@
  */
 package com.mysite.fitnesstracker.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,13 @@ import com.mysite.fitnesstracker.model.Exercise;
 @Controller
 public class MinutesController {
 
+	private static final Logger logger = Logger.getLogger(MinutesController.class);
+	
 	@RequestMapping("/addMinutes")
 	public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
+		logger.info("Entering MinutesController.addMinutes");
 		exercise.setMinutes(5);
+		logger.info("Exiting MinutesController.addMinutes");
 		return "minutes";
 	}
 }
