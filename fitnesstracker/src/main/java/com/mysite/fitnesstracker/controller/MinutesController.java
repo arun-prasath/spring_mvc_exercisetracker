d254f3fd6a0805e7eb1b05c907c992e40102defa
arun-prasath <arunprasath-dev@outlook.com>
2016-01-21 23:45:13 +0530

    Changes for REST Json response

--- fitnesstracker/src/main/java/com/mysite/fitnesstracker/controller/MinutesController.java
+++ fitnesstracker/src/main/java/com/mysite/fitnesstracker/controller/MinutesController.java
@@ -3,11 +3,18 @@
  */
 package com.mysite.fitnesstracker.controller;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mysite.fitnesstracker.model.Activity;
 import com.mysite.fitnesstracker.model.Exercise;
 
 /**
@@ -24,5 +31,24 @@ public class MinutesController {
 		exercise.setMinutes(5);
 		logger.info(exercise.getMinutes() + " minutes");
 		return "minutes";
+	}
+	
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> getAllActivities() {
+		List<Activity> activities = new ArrayList<Activity>();
+		
+		Activity walking = new Activity();
+		walking.setActivityName("Walking");
+		activities.add(walking);
+		
+		Activity biking = new Activity();
+		biking.setActivityName("Biking");
+		activities.add(biking);
+		
+		Activity swimming = new Activity();
+		swimming.setActivityName("Swimming");
+		activities.add(swimming);
+		
+		return activities;
 	}
 }
---
+++ fitnesstracker/src/main/java/com/mysite/fitnesstracker/model/Activity.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.mysite.fitnesstracker.model;
+
+/**
+ * @author Arun J
+ *
+ */
+public class Activity {
+	
+	private String activityName;
+
+	public String getActivityName() {
+		return activityName;
+	}
+
+	public void setActivityName(String activityName) {
+		this.activityName = activityName;
+	}
+
+}
