5a4666d7991303f48eba0ece7eccdc4307229439
arun-prasath <arunprasath-dev@outlook.com>
2016-01-23 12:36:03 +0530

    Changes to add Goal as a JPA entity

--- fitnesstracker/src/main/java/com/mysite/fitnesstracker/model/Goal.java
+++ fitnesstracker/src/main/java/com/mysite/fitnesstracker/model/Goal.java
@@ -3,19 +3,40 @@
  */
 package com.mysite.fitnesstracker.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import org.hibernate.validator.constraints.Range;
 
 /**
  * @author Arun J
  *
  */
+
+@Entity
 public class Goal {
-	
+
+	@Id
+	@GeneratedValue
+	@Column(name="GOAL_ID")
+	private Long goalId;
+
+	@Column(name="MINUTES")
 	@Range(min = 1, max = 120)
 	private int minutes;
 
+	public Long getGoalId() {
+		return goalId;
+	}
+
 	public int getMinutes() {
 		return minutes;
+	}
+
+	public void setGoalId(Long goalId) {
+		this.goalId = goalId;
 	}
 
 	public void setMinutes(int minutes) {
