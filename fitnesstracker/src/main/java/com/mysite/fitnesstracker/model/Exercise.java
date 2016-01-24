838032dd3ae4c537dbb34d5881ec1022661c8d91
arun-prasath <arunprasath-dev@outlook.com>
2016-01-24 17:45:32 +0530

    Added entity for exercise class

--- fitnesstracker/src/main/java/com/mysite/fitnesstracker/model/Exercise.java
+++ fitnesstracker/src/main/java/com/mysite/fitnesstracker/model/Exercise.java
@@ -1,11 +1,30 @@
 package com.mysite.fitnesstracker.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Exercise {
 
+	@Id
+	@GeneratedValue
+	@Column(name="EXERCISE_ID")
+	private long exerciseId;
+	
 	private int minutes;
+
+	public long getExerciseId() {
+		return exerciseId;
+	}
 
 	public int getMinutes() {
 		return minutes;
+	}
+
+	public void setExerciseId(long exerciseId) {
+		this.exerciseId = exerciseId;
 	}
 
 	public void setMinutes(int minutes) {
