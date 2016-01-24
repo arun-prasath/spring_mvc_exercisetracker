7dd47fbcb16c57f264125ed4809a17a9e4f6dfec
arun-prasath <arunprasath-dev@outlook.com>
2016-01-24 18:38:37 +0530

    Changed incorrect transaction annotation

--- fitnesstracker/src/main/java/com/mysite/fitnesstracker/repository/ExerciseRepositoryImpl.java
+++ fitnesstracker/src/main/java/com/mysite/fitnesstracker/repository/ExerciseRepositoryImpl.java
@@ -5,9 +5,9 @@ package com.mysite.fitnesstracker.repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mysite.fitnesstracker.model.Exercise;
 
